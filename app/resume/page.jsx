3b3814ec564afc58e3eafff18530ff5706974250
Bruno Pinto <167762364+BrunoMaiaPinto@users.noreply.github.com>
2024-10-18 11:47:50 +0100

    Add files via upload

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,42 +1,20 @@
-// const URL = "https://registry.jsonresume.org/BrunoMaiaPinto.json";
+import { getResume } from "../services/apiResume";
 
 export const metadata = { title: "Resume" };
 
-const skills = [
-  "HTML / JSX",
-  "CSS / SASS / Tailwind CSS",
-  "JavaScript",
-  "React / Next.js",
-];
-
-const education = [
-  {
-    degree: "Curso Tecnológico de Informática",
-    institution: "Escola Secundária João Gonçalves Zarco",
-    years: "2004 - 2008",
+const {
+  basics: {
+    name,
+    label,
+    location: { city, countryCode },
   },
-];
+  work,
+  education,
+  skills: teste,
+  languages,
+} = await getResume();
 
-const workExperience = [
-  {
-    title: "Meter Reader",
-    company: "Segurlight, Lda",
-    years: "2020 - 2024",
-    description: `Prepare the reading service daily and organize trips; Perform readings of meters (residential homes, businesses, etc.) and record values in designated forms or on a computer; Provide information to customers about reading services; Report any detected anomalies to the relevant services; Prepare and submit reading records.`,
-  },
-
-  {
-    title: "Meter Reader",
-    company: "Norleituras Unipessoal Lda.",
-    years: "2008 - 2020",
-    description: `Prepare the reading service daily and organize trips; Perform readings of meters (residential homes, businesses, etc.) and record values in designated forms or on a computer; Provide information to customers about reading services; Report any detected anomalies to the relevant services; Prepare and submit reading records.`,
-  },
-];
-
-const languages = [
-  { language: "Portuguese", proficiency: "Native Speaker" },
-  { language: "English", proficiency: "Fluent" },
-];
+const skills = teste.at(0).keywords;
 
 const Section = function ({ title, children }) {
   return (
@@ -67,14 +45,14 @@ const Skills = function () {
 const Education = function () {
   return (
     <ul className="flex justify-between ml-4">
-      {education.map(({ degree, institution, years }) => (
+      {education.map(({ area, institution, startDate, endDate }) => (
         <>
-          <li key={degree}>
-            <span className="font-semibold">{degree} </span>
+          <li key={area}>
+            <span className="font-semibold">{area} </span>
             <span>| {institution}</span>
           </li>
           <li>
-            <span className="font-bold">{years}</span>
+            <span className="font-bold">{`${startDate} - ${endDate}`}</span>
           </li>
         </>
       ))}
@@ -85,18 +63,18 @@ const Education = function () {
 const WorkExperience = function () {
   return (
     <div>
-      {workExperience.map(({ title, company, years, description }) => (
-        <div key={title}>
+      {work.map(({ position, name, startDate, endDate, summary }) => (
+        <div key={name}>
           <ul className="flex justify-between gap-1 ml-4">
             <li>
               <div>
-                <span className="font-semibold">{title}</span>
-                <span className="text-sm"> | {company}</span>
+                <span className="font-semibold">{position}</span>
+                <span className="text-sm"> | {name}</span>
               </div>
             </li>
-            <li className="font-bold">{years}</li>
+            <li className="font-bold">{`${startDate} - ${endDate}`}</li>
           </ul>
-          <p className="ml-5 mt-2 text-sm mb-3">{description}</p>
+          <p className="ml-5 mt-2 text-sm mb-3">{summary}</p>
         </div>
       ))}
     </div>
@@ -106,10 +84,10 @@ const WorkExperience = function () {
 const Languages = function () {
   return (
     <ul className="flex gap-8 ml-4">
-      {languages.map(({ language, proficiency }) => (
+      {languages.map(({ language, fluency }) => (
         <li key={language} className="flex flex-col">
           <span className="font-semibold">{language}</span>{" "}
-          <span className="text-xs italic">{proficiency}</span>
+          <span className="text-xs italic">{fluency}</span>
         </li>
       ))}
     </ul>
@@ -120,11 +98,11 @@ export default function page() {
   return (
     <div className="max-w-[820px] m-auto text leading-6">
       <div>
-        <h1 className="text-4xl font-thin">Bruno Pinto</h1>
-        <h2 className="text-xl">
-          Looking for an opportunity as Junior Front-End Web Developer
-        </h2>
-        <span>Porto, PT</span>
+        <h1 className="text-4xl font-thin">{name}</h1>
+        <h2 className="text-xl">{label}</h2>
+        <span>
+          {city}, {countryCode}
+        </span>
       </div>
 
       <Section title="Skills">
