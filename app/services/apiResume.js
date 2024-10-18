f8f06cf557a964bd9bd412fae13225e37ec0d5c8
Bruno Pinto <167762364+BrunoMaiaPinto@users.noreply.github.com>
2024-10-18 12:17:23 +0100

    Update apiResume.js

--- app/services/apiResume.js
+++ app/services/apiResume.js
@@ -1,6 +1,6 @@
 export const getResume = async function () {
   const res = await fetch(
-    "https://registry.jsonresume.org/BrunoMaiaPinto.json"
+    `https://registry.jsonresume.org/BrunoMaiaPinto.json?timestamp=${Date.now()}`
   );
   const data = await res.json();
   return data;
